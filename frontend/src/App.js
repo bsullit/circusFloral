f0aa4a7e924ccfe61c7397df1dfd9019fe6506ef
Brian Sullivan <brian.sullivan.ic#@gmail.com>
2023-03-06 15:00:31 -0500

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">circusFloral</a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
