36e9c956a70dc550239804b767bf39b36270d7b5
Brian Sullivan <brian.sullivan.ic#@gmail.com>
2023-03-16 16:04:48 -0400

    dummy data and listing products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,25 @@
+import data from './data';
+
+// const shmeta = {
+//   products: [{ name: 'dummy' }, { name: 'dummy2' }, { name: 'dummy3' }],
+// };
+
 function App() {
   return (
     <div>
       <header>
         <a href="/">circusFloral</a>
       </header>
-      <main>list products</main>
+      <main>
+        <h1>Featured Products</h1>
+        {data.products.map((product) => (
+          <div>
+            <img src={product.image} alt={product.name} />
+            <p>{product.name}</p>
+            <p>{product.price}</p>
+          </div>
+        ))}
+      </main>
     </div>
   );
 }
