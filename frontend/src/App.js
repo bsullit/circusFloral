ea2d167c23987f94d3f98dde94f9be3e91cdcc75
Brian Sullivan <brian.sullivan.ic#@gmail.com>
2023-03-17 21:51:44 -0400

    adding routing and homepage component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import data from './data';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 // const shmeta = {
 //   products: [{ name: 'dummy' }, { name: 'dummy2' }, { name: 'dummy3' }],
@@ -6,21 +7,30 @@ import data from './data';
 
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/">circusFloral</a>
-      </header>
-      <main>
-        <h1>Featured Products</h1>
-        {data.products.map((product) => (
-          <div>
-            <img src={product.image} alt={product.name} />
-            <p>{product.name}</p>
-            <p>{product.price}</p>
+    <BrowserRouter>
+      <div>
+        <header>
+          <a href="/">circusFloral</a>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<Homescreen />} />
+          </Routes>
+          <h1>Featured Products</h1>
+          <div className="products">
+            {data.products.map((product) => (
+              <div className="product" key={product.slug}>
+                <img src={product.image} alt={product.name} />
+                <div className="product-info">
+                  <p>{product.name}</p>
+                  <p>{product.price}</p>
+                </div>
+              </div>
+            ))}
           </div>
-        ))}
-      </main>
-    </div>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
