4d933f3a327a31517b061bf3e53b2c9bf476ea1c
Brian Sullivan <brian.sullivan.ic#@gmail.com>
2023-03-20 12:58:06 -0400

    basic styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import data from './data';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Homescreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
@@ -12,15 +13,21 @@ function App() {
         <main>
           <Routes>
             <Route path="/" element={<Homescreen />} />
+            <Route path="/product/:slug" element={<ProductScreen />} />
           </Routes>
           <h1>Featured Products</h1>
           <div className="products">
             {data.products.map((product) => (
               <div className="product" key={product.slug}>
-                <img src={product.image} alt={product.name} />
+                <a href={`/product/${product.slug}`}>
+                  <img src={product.image} alt={product.name} />
+                </a>
                 <div className="product-info">
-                  <p>{product.name}</p>
-                  <p>{product.price}</p>
+                  <a href={`/product/${product.slug}`}>
+                    <p>{product.name}</p>
+                  </a>
+                  <strong>{product.price}</strong>
+                  <button>Add to cart </button>
                 </div>
               </div>
             ))}
