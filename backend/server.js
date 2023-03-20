7e9a6f9e008778c6c46711c2c19a1afc7956b0e5
Brian Sullivan <brian.sullivan.ic#@gmail.com>
2023-03-20 14:52:12 -0400

    setting first api route

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
-import { Express } from 'express';
-import data from './data';
+import express from 'express';
+import data from './data.js';
 
 const app = express();
 
-app.get('api/products', (req, res) => {
-  req.send(data.products);
+app.get('/api/products', (req, res) => {
+  res.send(data.products);
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`serve at http://localhost:${port}`);
 });
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -3,7 +3,6 @@ import { useParams } from 'react-router-dom';
 function ProductScreen() {
   const params = useParams();
   const { slug } = params;
-  console.log(params);
 
   return <h1>{slug}</h1>;
 }
