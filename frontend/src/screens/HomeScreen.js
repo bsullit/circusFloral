ae275f15cda4dfbab33cf9a672ed7c119b3ca5e2
Brian Sullivan <brian.sullivan.ic#@gmail.com>
2023-03-21 14:49:30 -0400

    fetching product data from express server

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,12 +1,25 @@
-import data from '../data';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+
 function HomeScreen() {
+  //fetching all products from server
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get('/api/products');
+      setProducts(result.data);
+    };
+    fetchData();
+  }, []);
+
+  //need to fetch data from backend and call in homepage component
   return (
     <div>
       {' '}
       <h1>Featured Products</h1>
       <div className="products">
-        {data.products.map((product) => (
+        {products.map((product) => (
           <div className="product" key={product.slug}>
             <Link to={`/product/${product.slug}`}>
               <img src={product.image} alt={product.name} />
