f01f9dea4aa29de2f6b92d84256da966f2bc3ea5
Brian Sullivan <brian.sullivan.ic#@gmail.com>
2023-03-22 13:47:52 -0400

    adding react boostrap and react bootstrap routing to app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,22 @@ import data from './data';
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import Homescreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function App() {
   return (
     <BrowserRouter>
       <div>
         <header>
-          <Link to="/">circusFloral</Link>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>circusFloral</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
         <main>
           <Routes>
